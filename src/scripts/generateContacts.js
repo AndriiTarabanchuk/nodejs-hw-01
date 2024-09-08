339a84ed03f5f55624dcd7c1d073f84ff6ecc89f
AndreyTara <explorituse@gmail.com>
2024-09-08 15:10:33 +0300

    'adds-faker'

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,10 @@
-const generateContacts = async (number) => {};
+// import { faker } from '@faker-js/faker';
+// import { createFakeContact } from '../utils/createFakeContact';
+
+const generateContacts = async (number) => {
+  // return faker.helpers.multiple(createFakeContact, {
+  //   count: number,
+  // });
+};
 
 generateContacts(5);
