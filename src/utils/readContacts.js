a5c3b9cf62e11b379506fe029f425b878ba8111b
AndreyTara <explorituse@gmail.com>
2024-09-08 18:35:12 +0300

    'adds-read-write-utils'

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,11 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
+
 export const readContacts = async () => {
-  // try {
-  //   const contacts = await fs.readFile(PATH_DB);
-  //   console.log(await contacts.toString());
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  try {
+    const contacts = await fs.readFile(PATH_DB);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log('Error while read contacts.', error);
+  }
 };
-readContacts();
+
+// console.log(await readContacts());
