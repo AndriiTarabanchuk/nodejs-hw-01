1b1b294d85922099bac0af79a81050a17fe2615a
AndreyTara <explorituse@gmail.com>
2024-09-09 20:05:29 +0300

    'fix-generate'

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,13 +1,15 @@
 import { faker } from '@faker-js/faker';
 import { createFakeContact } from '../utils/createFakeContact.js';
 import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
 
 const generateContacts = async (number) => {
   try {
+    const contacts = await readContacts();
     const newContacts = faker.helpers.multiple(createFakeContact, {
       count: number,
     });
-    await writeContacts(newContacts);
+    await writeContacts(contacts.concat(newContacts));
   } catch (error) {
     console.log('Error while generate contacts.', error);
   }
